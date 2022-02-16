625a7c45b4dc12fe4b2c6206c4b6565c4f98269c
pusann <rainbow.7.7west2014@gmail.com>
2022-02-16 20:25:25 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
   resources :articles,  only: [:index, :new,:create]
-  resources :message,   only: [:index,:create]
+  resources :messages,   only: [:index,:create]
   resources :rooms,  only: [:new,:create,:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
